5f6239b27e5d0b84e0282eedfe95c756ed748988
MrConorAE <26195325+MrConorAE@users.noreply.github.com>
2019-12-24 13:33:06 +1300

    Fixed a few things

--- script.js
+++ script.js
@@ -51,50 +51,54 @@ function gotResult(error, results) {
 
 function displayResult(results) {
   //Create
-  var newlabel = document.createElement("p");
-  newlabel.classList = "labels hide";
-  newlabel.innerHTML = results[0].label;
-  labels.push(newlabel);
-  document.getElementById('labels-cont').appendChild(newlabel);
-  //Display
-  setTimeout(function () {
-    labels[labels.length-2].classList = "labels hide";
-    labels[labels.length-1].classList = "labels";
-
-    //Confidence
-    //Confidence
-    document.getElementById('conf').innerHTML = ((Math.round((results[0].confidence + Number.EPSILON) * 100) / 100) * 100) + "% confidence";
-    document.getElementById('conf').style = ("color: hsl(" + ((Math.round((results[0].confidence + Number.EPSILON)*100)/100)*100) + ", 100%, 50%); text-shadow: hsl(" + ((Math.round((results[0].confidence + Number.EPSILON)*100)/100)*100) + ", 100%, 50%) 0px 0px 5px;");
-  
-    //Remove old
+  if (results[0].label != labels[labels.length - 1].innerHTML) {
+    var newlabel = document.createElement("p");
+    newlabel.classList = "labels hide-b";
+    newlabel.innerHTML = results[0].label;
+    labels.push(newlabel);
+    document.getElementById('labels-cont').appendChild(newlabel);
+    //Display
     setTimeout(function () {
-     document.getElementById('labels-cont').removeChild(labels[0]);
-     labels.shift();
+      labels[labels.length - 2].classList = "labels hide-t";
+      labels[labels.length - 1].classList = "labels";
+      
+      //Confidence
+      document.getElementById('conf').innerHTML = nf(results[0].confidence*100, 0, 1) + "% confidence";
+      document.getElementById('conf').style = ("color: hsl(" + nf(results[0].confidence*100, 0, 1) + ", 100%, 50%); text-shadow: hsl(" + nf(results[0].confidence*100, 0, 1) + ", 100%, 50%) 0px 0px 5px;");
+
+      //Remove old
+      setTimeout(function () {
+        document.getElementById('labels-cont').removeChild(labels[0]);
+        labels.shift();
+      }, 300);
     }, 300);
-  }, 300);
-  
+  } else {
+    //Only update confidence
+    document.getElementById('conf').innerHTML = nf(results[0].confidence*100, 0, 1) + "% confidence";
+    document.getElementById('conf').style = ("color: hsl(" + nf(results[0].confidence*100, 0, 1) + ", 100%, 50%); text-shadow: hsl(" + nf(results[0].confidence*100, 0, 1) + ", 100%, 50%) 0px 0px 5px;");
+  }
 }
 
 function onload() {
-    if (navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({
-          video: true
-        })
-        .then(function (stream) {
-          video.srcObject = stream;
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-    }
-  
-    var newlabel = document.createElement("p");
-    newlabel.classList = "label hide";
-    newlabel.innerHTML = "...";
-    labels.push(newlabel);
-    document.getElementById('labels-cont').appendChild(newlabel);
-  
-    setInterval(function () {
-      classifier.classify(gotResult);
-    }, 1000);
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({
+        video: true
+      })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
+  var newlabel = document.createElement("p");
+  newlabel.classList = "labels";
+  newlabel.innerHTML = "...";
+  labels.push(newlabel);
+  document.getElementById('labels-cont').appendChild(newlabel);
+
+  setInterval(function () {
+    classifier.classify(gotResult);
+  }, 1000);
 }
